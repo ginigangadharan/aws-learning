66692107e0b3f3604f8b458c8a0bbb842bcd4ce3
Gini <net.gini@gmail.com>
2019-06-17 17:45:17 +0800

    Create lambda-sample.py

---
+++ lambda-sample.py
@@ -0,0 +1,10 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "colovibes"
+    }
